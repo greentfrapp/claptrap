13027ffa3da4c83773841de86534f03af69cebc9
greentfrapp <limsweekiat@gmail.com>
2017-04-09 20:52:13 +0800

    Updated random-search.py

--- gym-Cartpole-v0/random-search.py
+++ gym-Cartpole-v0/random-search.py
@@ -4,23 +4,25 @@ import numpy as np
 
 def main():
   env = gym.make('CartPole-v0')
-  env = wrappers.Monitor(env, '/tmp/cartpole-experiment-1',force=True)
+  log_directory = '/tmp/cartpole-random-search'
+  env = wrappers.Monitor(env,log_directory,force=True)
+
   best_reward = 0
   best_param = None
-  for _ in range(500):
+
+  for _ in range(1000):
     param = np.random.rand(4)*2-1
     reward = run_env(env,param)
     if reward>best_reward:
       best_param = param
       best_reward = reward
     if best_reward==200: break
-  print best_reward
 
+  # CartPole-v0 defines "solving" as getting average reward of 195.0 over 100 consecutive trials.
   for _ in range(100):
-    run_env(env,best_param,no_of_steps=200)
+    run_env(env,best_param)
 
   env.close()
-
 
 def run_env(env,param,no_of_steps=200,show=False):
   observation = env.reset()
