951b0b0863c6819237dd2612924395e0563b674a
greentfrapp <limsweekiat@gmail.com>
2017-04-11 15:51:23 +0800

    Rewrote policy-gradient.py but we are still missing the optimizers for both Policy() and RewardModel().

--- gym-Cartpole-v0/policy-gradient.py
+++ gym-Cartpole-v0/policy-gradient.py
@@ -1,158 +1,120 @@
 import gym
 from gym import wrappers
 import numpy as np
-import matplotlib.pyplot as plt
+
+class Policy(object):
+
+  def __init__(self):
+    w1 = np.random.randn(2,4)
+    self.weights = {
+      'w1':w1,
+      }
+
+  def get_action(self,state):
+    output = np.matmul(self.weights['w1'],state)
+    action_probabilities = softmax(output)
+    action = 0 if random.uniform(0,1) < action_probabilities[0] else 1
+    return action
+
+  def update(self,performance,history):
+
+
+class RewardModel(object):
+
+  def __init__(self):
+    # Initialize weights
+    w1 = np.random.randn(10,4) / np.sqrt(4)
+    w2 = np.random.randn(1,10) / np.sqrt(10)
+    b1 = np.zeros((10,1))
+    b2 = np.zeros((1,1))
+    self.weights = {
+      'w1':w1,
+      'b1':b1,
+      'w2':w2,
+      'b2':b2
+      }
+
+  def predict_cumulative_reward(self,history):
+    cumulative_reward_history = []
+    for step in history:
+      cumulative_reward = np.matmul(self.weights['w2'],relu(np.matmul(self.weights['w1'],step['prev_state'])+self.weights['b1']))+self.weights['b2']
+      cumulative_reward_history.append(cumulative_reward)
+    return cumulative_reward_history
+
+  def update(self,true_cumulative_reward,history)
 
 def main():
   env = gym.make('CartPole-v0')
-  #log_directory = '/tmp/cartpole-hill-climbing'
+  log_directory = '/tmp/cartpole-policy-gradient'
   #env = wrappers.Monitor(env,log_directory,force=True)
 
-  total_reward = 0
+  policy = Policy()
+  reward_model = RewardModel()
 
-  # Learning parameters
-  episode_no = 0
-  batch_size = 10
-  decay_rate = 0.9
-  learning_rate = 1e-3
-  running_avg_reward = 0
-  batch_reward = []
+  # Run at most 1000 episodes for training
+  for _ in range(1000):
+    total_reward = run_episode(env,reward_model,policy)
+    if total_reward == 200: break
 
-  # Neural network parameters
-  num_hidden_layer_neurons = 10
-  input_dimensions = 4
-  weights = {
-    '1': np.random.randn(num_hidden_layer_neurons,input_dimensions)/np.sqrt(input_dimensions),
-    '2': np.random.randn(num_hidden_layer_neurons)/np.sqrt(num_hidden_layer_neurons)
-  }
-
-  # Plot axes
-  episodes = []
-  rewards = []
-
-  # START - TOREAD
-  expectation_g_squared = {}
-  g_dict = {}
-  for layer_name in weights.keys():
-    expectation_g_squared[layer_name] = np.zeros_like(weights[layer_name])
-    g_dict[layer_name] = np.zeros_like(weights[layer_name])
-  # END - TOREAD
-
-  episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = [], [], [], []
-
-  observation = env.reset()
-
-  while episode_no<=10000:
-
-    env.render()
-
-    hidden_layer_values,action_probability = boomboom(observation, weights)
-
-    episode_observations.append(observation)
-    episode_hidden_layer_values.append(hidden_layer_values)
-
-    action = choose_action(action_probability)
-
-    observation,reward,done,info = env.step(action)
-
-    total_reward += reward
-    episode_rewards.append(total_reward)
-
-    fake_label = 1 if action == 1 else 0
-    loss_function_gradient = fake_label - action_probability
-    episode_gradient_log_ps.append(loss_function_gradient)
-
-    if done:
-
-      episode_no += 1
-
-      episode_hidden_layer_values = np.stack(episode_hidden_layer_values,0)
-      episode_observations = np.stack(episode_observations,0)
-      episode_gradient_log_ps = np.stack(episode_gradient_log_ps,0)
-      episode_rewards = np.stack(episode_rewards,0)
-
-      episode_gradient_log_ps = discount_with_rewards(episode_gradient_log_ps,episode_rewards,running_avg_reward)
-
-      gradient = get_gradient(episode_gradient_log_ps,episode_hidden_layer_values,episode_observations,weights)
-
-      for layer_name in gradient: g_dict[layer_name] += gradient[layer_name]
-      if episode_no % batch_size == 0:
-        update_weights(weights, expectation_g_squared, g_dict, decay_rate, learning_rate)
-        running_avg_reward = np.mean(batch_reward)
-
-      #print weights['1'][0]
-      print total_reward
-
-      episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = [], [], [], [] # reset values
-
-      episodes.append(episode_no)
-      rewards.append(total_reward)
-      batch_reward.append(total_reward)
-      total_reward = 0
-      observation = env.reset()
+  # Run 100 more episodes for proving that exercise is solved
+  for _ in range(100):
+    run_episode(env,reward_model,policy)
 
   env.close()
 
-  ax = plt.axes()
-  ax.plot(episodes,rewards)
-  plt.show()
+def run_episode(env,reward_model,policy,no_of_steps=200):
 
-def sigmoid(x):
-  return 1.0/(1.0 + np.exp(-x))
+  history = []
+  total_reward = 0
+
+  state = env.reset()
+
+  for _ in range(no_of_steps):
+    prev_state = state
+    # Get action probabilities, select and run action
+    action = policy.get_action(state)
+    state, reward, done, info = env.step(action)
+
+    history.append({'prev_state':prev_state,'action':action,'reward':reward})
+    total_reward += reward
+
+    if done: break
+
+  # Calculate the true and predicted cumulative rewards
+  true_cumulative_reward = get_cumulative_reward(history)
+  predicted_cumulative_reward = reward_model.predict_cumulative_reward(history)
+
+  # Calculate performance of the policy
+  performance = predicted_cumulative_reward - true_cumulative_reward
+
+  # Use true_cumulative_reward and history to update reward_model
+  reward_model.update(true_cumulative_reward,history)
+
+  # Use performance and history to update policy
+  policy.update(performance,history)
+
+  return total_reward
 
 def relu(vector):
   vector[vector < 0] = 0
   return vector
 
-def boomboom(observation,weights):
-  hidden_layer_values = np.dot(weights['1'], observation)
-  hidden_layer_values = relu(hidden_layer_values)
-  output_layer_values = np.dot(hidden_layer_values, weights['2'])
-  output_layer_values = sigmoid(output_layer_values)
-  return hidden_layer_values,output_layer_values
+def softmax(vector):
+  vector = np.exp(vector)
+  vector /= np.sum(vector)
+  return vector
 
-def choose_action(probability):
-  random_value = np.random.uniform()
-  if random_value < probability:
-    return 0
-  else:
-    return 1
-
-def discount_with_rewards(gradient_log_p, episode_rewards, threshold):
-  """ discount the gradient with the normalized rewards """
-  discounted_episode_rewards = episode_rewards
-  # standardize the rewards to be unit normal (helps control the gradient estimator variance)
-  #if discounted_episode_rewards[-1]<threshold:
-  #  discounted_episode_rewards = -discounted_episode_rewards
-  #discounted_episode_rewards -= np.mean(discounted_episode_rewards)
-  #discounted_episode_rewards /= np.std(discounted_episode_rewards)
-  #return gradient_log_p * discounted_episode_rewards
-  if discounted_episode_rewards[-1]<threshold: return -gradient_log_p * (threshold - discounted_episode_rewards[-1])
-  else: return gradient_log_p * (discounted_episode_rewards[-1])
-
-def get_gradient(gradient_log_p, hidden_layer_values, observation_values, weights):
-  # START - TOREAD
-  delta_L = gradient_log_p
-  dC_dw2 = np.dot(hidden_layer_values.T, delta_L).ravel()
-  delta_l2 = np.outer(delta_L, weights['2'])
-  delta_l2 = relu(delta_l2)
-  dC_dw1 = np.dot(delta_l2.T, observation_values)
-  # END - TOREAD
-  return {
-      '1': dC_dw1,
-      '2': dC_dw2
-  }
-
-def update_weights(weights, expectation_g_squared, g_dict, decay_rate, learning_rate):
-  #print g_dict
-  #print weights
-  # START - TOREAD
-  epsilon = 1e-5
-  for layer_name in weights.keys():
-    g = g_dict[layer_name]
-    expectation_g_squared[layer_name] = decay_rate * expectation_g_squared[layer_name] + (1 - decay_rate) * g**2
-    weights[layer_name] += (learning_rate * g)/(np.sqrt(expectation_g_squared[layer_name] + epsilon))
-    g_dict[layer_name] = np.zeros_like(weights[layer_name]) # reset batch gradient buffer
-   # END - TOREAD
+def get_cumulative_reward(history):
+  cumulative_reward_history = []
+  for idx,step in enumerate(history):
+    no_of_steps_left = len(history)-idx
+    cumulative_reward = 0
+    discount = 1.00
+    discount_decrement = 0.95
+    for idx2,step in enumerate(history[idx:]):
+      cumulative_reward += step['reward'] * discount
+      discount *= discount_decrement
+    cumulative_reward_history.append(cumulative_reward)
+  return np.array(cumulative_reward)
 
 if __name__=="__main__": main()
